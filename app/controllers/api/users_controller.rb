a9e5be3238c40b5da0362160727b706735e2c2c8
Henrique Okomura <henrique.okomura@dev.codus.com.br>
2016-01-29 15:09:56 -0200

    Users controller specs

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,20 +2,23 @@ class Api::UsersController < Api::BaseController
   # respond_to :json
 
   def create
-    respond_with :api, User.create!(permitted_params)
+    @user = User.create!(permitted_params)
+    respond_with :api, @user
   end
 
   def show
-    respond_with User.find(params[:id])
+    render json: User.find(params[:id])
   end
 
   def update
-    adventure = User.find(params[:id])
-    respond_with adventure.update!(permitted_params)
+    @user = User.find(params[:id])
+    @user.update!(permitted_params)
+    respond_with :api, @user
   end
 
   def destroy
-    respond_with User.destroy!(params[:id])
+    User.find(params[:id]).destroy!(params[:id])
+    render json: {}
   end
 
   private
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,17 +2,62 @@ require 'spec_helper'
 
 describe Api::UsersController, :type => :controller do
   let(:user) { FactoryGirl.create(:user) }
-  let(:advendures) { FactoryGirl.create_list(:adventure, 3)}
 
   describe "POST #create" do
     let(:new_user_name) { "Henrique" }
-    it "creates a user" do
-      params = {user: { name: new_user_name, adventure_ids: advendures.map(&:id) } }
-      post :create, params
+    let(:adventures_ids) { FactoryGirl.create_list(:adventure, 3).map(&:id) }
 
-      byebug
+    before(:each) do
+      params = {user: { name: new_user_name, adventure_ids: adventures_ids } }
+      post :create, params
     end
 
+    it "creates a user" do
+      new_user = assigns(:user)
+
+      expect(new_user.name).to eq new_user_name
+      adventures_ids.each { |adventure_id| expect(new_user.adventure_ids).to include adventure_id }
+    end
+
+    it "redirects to GET #show" do
+      new_user = assigns(:user)
+      expect(response).to redirect_to api_user_path(id: new_user.id)
+    end
+
+  end
+
+  describe "GET #show" do
+    it "respond with the users json" do
+      get :show, id: user.id
+      expect(response.body).to eq user.to_json
+    end
+  end
+
+  describe "PATCH #update" do
+    let(:new_user_name) { "Henrique" }
+    let(:adventures_ids) { FactoryGirl.create_list(:adventure, 3).map(&:id) }
+
+    before(:each) do
+      patch :update, id: user.id, user: { name: new_user_name, adventure_ids: adventures_ids }
+    end
+
+    it "respond with the users json" do
+      user.reload
+
+      expect(user.name).to eq new_user_name
+      adventures_ids.each { |adventure_id| expect(user.adventure_ids).to include adventure_id }
+    end
+
+    it "redirects to GET #show" do
+      expect(response).to redirect_to api_user_path(id: user.id)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the given user" do
+      delete :destroy, id: user.id
+      expect(User.where(id: user.id).count).to eq 0
+    end
   end
 
 end
