d0bcb48d1020238e5b5b9d39079a6bf392fbcaf1
Henrique Okomura <henrique.okomura@dev.codus.com.br>
2016-02-24 16:29:55 -0300

    Adde some comments

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -5,9 +5,11 @@ class Adventure
   # Fields
   field :name, type: String
 
+  #Associations
   belongs_to :master, class_name: "User"
   has_and_belongs_to_many :players, class_name: "User"
   embeds_many :sheets
 
+  #Validations
   validates :master, presence: true
 end
